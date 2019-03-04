7846af59e675edbba527e2fcd0fc31b5f5a0509b
qq734628996 <734628996@qq.com>
2019-03-04 21:13:08 +0800

    提交作业2.1

---
+++ hw2/hw2.1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        public void print()
+        {
+            Console.WriteLine("实例化Hello World");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld helloWorld = new HelloWorld();
+            helloWorld.print();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ hw2/hw2.1/readme.md
@@ -0,0 +1,6 @@
+# 实例化 HelloWorld
+**作业要求**：创建新的类，实例化 HelloWorld
+
+**思路**：如题
+
+**结果**：![Alt text](//pic/res.png)
