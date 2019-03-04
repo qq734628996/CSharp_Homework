ffe751bc5fbb61d3a56f9d846c3fc8f1dedd3839
qq734628996 <734628996@qq.com>
2019-03-04 21:39:04 +0800

    提交了作业2.2

---
+++ hw2/hw2.2/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        public static void print(string[] args)
+        {
+            Console.WriteLine("实例化Hello World");
+            Console.WriteLine("The length of args is {0}", args.Length);
+            foreach (string i in args)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld.print(args);
+            Console.ReadLine();
+        }
+    }
+}
