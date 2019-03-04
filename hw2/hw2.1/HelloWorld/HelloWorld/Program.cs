9ecf995ad3ce68634a55de405571c3ecd4a8ed01
qq734628996 <734628996@qq.com>
2019-03-04 21:32:10 +0800

    添加了命令行参数

--- hw2/hw2.1/HelloWorld/HelloWorld/Program.cs
+++ hw2/hw2.1/HelloWorld/HelloWorld/Program.cs
@@ -4,9 +4,15 @@ namespace HelloWorld
 {
     class HelloWorld
     {
-        public void print()
+        public void print(string[] args)
         {
             Console.WriteLine("实例化Hello World");
+            Console.WriteLine("The length of args is {0}", args.Length);
+            foreach (string i in args)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("");
         }
     }
     class Program
@@ -14,7 +20,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             HelloWorld helloWorld = new HelloWorld();
-            helloWorld.print();
+            helloWorld.print(args);
             Console.ReadLine();
         }
     }
