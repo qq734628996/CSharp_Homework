4879a1c7f0207017512e466c18e6f3af98e7468b
qq734628996 <734628996@qq.com>
2019-03-05 18:54:12 +0800

    提交作业2.4

---
+++ hw2/hw2.4/HelloWindowsForms/HelloWindowsForms/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWindowsForms
+{
+    class HelloWorld
+    {
+        public HelloWorld()
+        {
+            MessageBox.Show("《象曰》：天行健，君子以自强不息。\n", "这是一个不起眼的messagebox");
+        }
+    }
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new HelloWorld();
+        }
+    }
+}
---
+++ hw2/hw2.4/readme.md
@@ -0,0 +1,13 @@
+# WindowsForm，中创建新类的HelloWorld
+又是拖控件……
+
+先放图：
+
+![](pic/res.png)
+
+1. 需要创建新类，于是我就建了一个新类。。
+2. 用了一个button和messagebox
+
+放个视频：（~~测试下视频效果~~
+
+![](video/res.mp4)
